c2a2c6c01b0d4a1534c0f5564d8a70ca9c9de3f9
KhinYadanarTun <KhinYadanarTun2025@gmail.com>
2018-06-26 12:48:37 +0630

    ex101.py

---
+++ ex101.py
@@ -0,0 +1,15 @@
+teddy_dog ="\t I'm tabbled in."
+persian_dog ="I'm split\non a line."
+blackslash_dog ="I'm \\ a \\ cat."
+
+fat_dog = """
+I'll do a list:
+    \t= dog food
+    \t= fishies
+    \t= Dognip\n\t= Grass
+    """
+
+print(tabby_dog)
+print(persian_dog)
+print(backslash_dog)
+print(fat_dog)
